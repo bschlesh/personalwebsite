cf633d2db15efb263feaab09a930ab5023c5c8dd
bschlesinger <bschlesinger@kbra.com>
2020-12-24 13:10:44 -0500

    Added particle background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Particles from './components/Particles.jsx'
+
+const styles = {
+  root: {
+    fontFamily: "sans-serif",
+    textAlign: "center",
+    height: "100%",
+    background: "#222",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={styles.root}>
+        <Particles />
+      </div>
   );
 }
 
